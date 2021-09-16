46ea39dc1843b77289180944a31409edf7c300ec
Gabriel <gabriel.knies@epitech.eu>
2021-09-16 15:19:44 +0200

    adding file upload

--- PROTO-WEBSITE/src/ts/appController.ts
+++ PROTO-WEBSITE/src/ts/appController.ts
@@ -1,6 +1,8 @@
 import * as ko from "knockout";
 import * as ResponsiveUtils from "ojs/ojresponsiveutils";
 import * as ResponsiveKnockoutUtils from "ojs/ojresponsiveknockoututils";
+import { whenDocumentReady } from "ojs/ojbootstrap";
+import { FilePickerElement } from "ojs/ojfilepicker";
 import "ojs/ojinputtext";
 import "ojs/ojlabel";
 import "ojs/ojbutton";
@@ -11,6 +13,8 @@ class RootViewModel {
   smScreen: ko.Observable<boolean>;
   appName: ko.Observable<string>;
   textInput: ko.Observable<string>;
+  isDisabled: ko.Observable<boolean>;
+  file: ko.Observable<FileList>;
 
   constructor() {
     // media queries for repsonsive layouts
@@ -19,14 +23,58 @@ class RootViewModel {
       this.smScreen = ResponsiveKnockoutUtils.createMediaQueryObservable(smQuery);
     }
     this.textInput = ko.observable('');
+    this.isDisabled = ko.observable(false);
+    this.file = ko.observable();
     // header
 
     // application Name used in Branding Area
     this.appName = ko.observable("Robotarium EIP Prototype Website");
   }
 
-  public submitCode = () : void => {
+  public selectListener = (event: FilePickerElement.ojSelect) : void => {
+    const files: FileList = event.detail.files;
+    this.file(files);
+  }
+
+  public submitCode = async () : Promise<void> => {
+    this.isDisabled(true);
     console.log(this.textInput());
+
+    if (this.textInput() === '' || this.textInput() == null) {
+      let body = {
+        "code": this.textInput(),
+      }
+      let response: Response = await fetch('url', {
+        method: 'POST',
+        mode: 'cors',
+        cache: 'no-cache',
+        credentials: 'same-origin',
+        headers: {
+          "content-type" : "application/json;charset=UTF-8",
+          "access-control-allow-origin": "*"
+        },
+        body: JSON.stringify(body),
+      })
+    } else {
+      const formData: FormData = new FormData();
+      formData.append('myFile', this.file().item[0]);
+  
+      let response = await fetch('url', {
+        method: 'POST',
+        mode: 'cors',
+        cache: 'no-cache',
+        credentials: 'same-origin',
+        headers: {
+          "content-type" : "application/json;charset=UTF-8",
+          "access-control-allow-origin": "*"
+        },
+        body: formData,
+      });
+      let res = await response.json();
+      console.log(res);
+    }
+
+    this.isDisabled(false);
   }
 
 }
